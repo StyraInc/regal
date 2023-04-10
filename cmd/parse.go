3b571f0c141e7cdf7431fa41a1520a4673991c3a
Anders Eknert <anders@styra.com>
2023-04-10 15:24:02 +0200

    Add regal parse command
    Allow parsing Rego with Regal additions
    
    Fixes #27
    
    Signed-off-by: Anders Eknert <anders@styra.com>

---
+++ cmd/parse.go
@@ -0,0 +1,72 @@
+//nolint:wrapcheck
+package cmd
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+
+	"github.com/open-policy-agent/opa/ast"
+	"github.com/spf13/cobra"
+	rp "github.com/styrainc/regal/internal/parse"
+)
+
+func init() {
+	parseCommand := &cobra.Command{
+		Use:   "parse <path> [path [...]]",
+		Short: "Parse Rego source files with Regal enhancements included in output",
+		Long:  "This command works similar to `opa parse` but includes Regal enhancements in the AST output.",
+
+		PreRunE: func(_ *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				return errors.New("no file to parse provided")
+			}
+
+			if len(args) > 1 {
+				return errors.New("only one file can be parsed at a time")
+			}
+
+			return nil
+		},
+
+		Run: func(_ *cobra.Command, args []string) {
+			if err := parse(args); err != nil {
+				log.SetOutput(os.Stderr)
+				log.Println(err)
+				os.Exit(1)
+			}
+		},
+	}
+	RootCommand.AddCommand(parseCommand)
+}
+
+func parse(args []string) error {
+	filename := args[0]
+
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	content := string(bs)
+
+	module, err := ast.ParseModuleWithOpts(filename, content, rp.ParserOptions())
+	if err != nil {
+		return err
+	}
+
+	enhancedAST, err := rp.EnhanceAST(filename, content, module)
+	if err != nil {
+		return err
+	}
+
+	bs, err = json.MarshalIndent(enhancedAST, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, err = os.Stdout.Write(bs)
+
+	return err
+}

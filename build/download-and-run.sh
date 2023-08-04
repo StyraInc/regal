13771bc4a7fecb5e81f9b0e32158dfdbba0e5795
Christian Wygoda <christian.wygoda@satellitevu.com>
2023-08-04 09:26:11 +0200

    Add self-downloading pre-commit hook

---
+++ build/download-and-run.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# pre-commit helper to run download latest release binary if missing before executing
+# linting with it.
+
+set -e
+
+REPO=StyraInc/regal
+BASE_URL=https://github.com/${REPO}
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+BIN_PATH="${SCRIPTPATH}/regal"
+
+download()
+{
+    DETECTED_SYSTEM=$(uname -s)
+    DETECTED_ARCHITECTURE=$(uname -m)
+
+    REGAL_VERSION=${REGAL_VERSION:-latest}
+    SYSTEM=${REGAL_SYSTEM:-${DETECTED_SYSTEM}}
+    ARCHITECTURE=${REGAL_ARCHITECTURE:-${DETECTED_ARCHITECTURE}}
+
+    echo "Downloading regal for ${SYSTEM} ${ARCHITECTURE}, ${REGAL_VERSION}…"
+    BINARY_URL=${BASE_URL}/releases/${REGAL_VERSION}/download/regal_${SYSTEM}_${ARCHITECTURE}
+    curl --fail -Lo "${BIN_PATH}" ${BINARY_URL}
+    chmod +x "${BIN_PATH}"
+}
+
+if [ ! -x "${BIN_PATH}" ]; then download; fi
+"${BIN_PATH}" $@

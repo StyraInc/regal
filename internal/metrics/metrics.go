995ca6a8e0722f9d3dfd6172a42a5cb1c4985c3d
Anders Eknert <anders@styra.com>
2023-09-19 09:52:40 +0200

    Metrics (#333)
    Add Regal-specific metrics in order to know what's taking
    the most time in linter evaluation.
    
    Also:
    * Pass metrics to EvalMetrics to get insights from OPA too
    * Have Publish functions accept a context. Unused for now
      but could be useful later.
    
    Fixes #26
    
    Signed-off-by: Anders Eknert <anders@styra.com>

---
+++ internal/metrics/metrics.go
@@ -0,0 +1,12 @@
+package metrics
+
+const (
+	RegalConfigSearch         = "regal_config_search"
+	RegalConfigParse          = "regal_config_parse"
+	RegalFilterIgnoredFiles   = "regal_filter_ignored_files"
+	RegalFilterIgnoredModules = "regal_filter_ignored_modules"
+	RegalInputParse           = "regal_input_parse"
+	RegalLint                 = "regal_lint_total"
+	RegalLintGo               = "regal_lint_go"
+	RegalLintRego             = "regal_lint_rego"
+)

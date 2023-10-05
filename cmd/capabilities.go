fa4712f396202c3f5c3cd1ad2637b772a0f24fa1
Anders Eknert <anders@styra.com>
2023-10-05 16:03:21 +0200

    Add capabilities.json and use for `opa check --strict` (#374)
    Fixes #369
    
    Signed-off-by: Anders Eknert <anders@styra.com>

---
+++ cmd/capabilities.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/styrainc/regal/internal/compile"
+)
+
+func init() {
+	capabilitiesCommand := &cobra.Command{
+		Hidden: true,
+		Use:    "capabilities",
+		Short:  "Print the capabilities of Regal",
+		Long:   "Show capabilities for Regal",
+		RunE: func(*cobra.Command, []string) error {
+			bs, err := json.MarshalIndent(compile.Capabilities(), "", "  ")
+			if err != nil {
+				return fmt.Errorf("failed marshalling capabilities: %w", err)
+			}
+
+			fmt.Fprintln(os.Stdout, string(bs))
+
+			return nil
+		},
+	}
+
+	RootCommand.AddCommand(capabilitiesCommand)
+}

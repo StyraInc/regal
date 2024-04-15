bf48f1108c3d84bef38d5149ad83f68ee738499f
Charlie Egan <charlie@styra.com>
2024-04-15 11:32:52 +0100

    lsp: further reduce the config watch timeout
    Signed-off-by: Charlie Egan <charlie@styra.com>

--- internal/lsp/config/watcher_test.go
+++ internal/lsp/config/watcher_test.go
@@ -39,7 +39,7 @@ foo: bar
 
 	select {
 	case <-watcher.Reload:
-	case <-time.After(5 * time.Second):
+	case <-time.After(100 * time.Millisecond):
 		t.Fatal("timeout waiting for initial config event")
 	}
 

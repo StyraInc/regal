e8935487986882e6360c8b9de3b4b08d747e2e2f
Charles Daniels <charles@styra.com>
2024-08-29 11:06:07 -0400

    implement PR review suggestions

--- internal/capabilities/capabilities_test.go
+++ internal/capabilities/capabilities_test.go
@@ -33,25 +33,6 @@ func TestLookupFromFile(t *testing.T) {
 	}
 }
 
-func TestLookupFromURL(t *testing.T) {
-	t.Parallel()
-
-	// Test that we can load a one of the existing OPA capabilities files
-	// via GitHub.
-
-	caps, err := Lookup(
-		context.Background(),
-		"https://raw.githubusercontent.com/open-policy-agent/opa/main/capabilities/v0.55.0.json",
-	)
-	if err != nil {
-		t.Errorf("unexpected error from Lookup: %v", err)
-	}
-
-	if len(caps.Builtins) != 193 {
-		t.Errorf("OPA v0.55.0 capabilities should have 193 builtins, not %d", len(caps.Builtins))
-	}
-}
-
 func TestLookupFromEmbedded(t *testing.T) {
 	t.Parallel()
 

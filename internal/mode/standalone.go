5f38a4bb943730247f6d950c378360494c7272d7
Stephan Renatus <stephan@styra.com>
2024-09-06 10:32:43 +0200

    build: introduce regal_standalone build flag, use for lint's "fix" hint
    Signed-off-by: Stephan Renatus <stephan@styra.com>

---
+++ internal/mode/standalone.go
@@ -0,0 +1,8 @@
+//go:build !regal_standalone
+
+package mode
+
+// Standalone lets us change the output of some commands when Regal
+// us used as a binary, as opposed to when it's embedded via its
+// Go module.
+const Standalone = false
---
+++ internal/mode/standalone_flag.go
@@ -0,0 +1,5 @@
+//go:build regal_standalone
+
+package mode
+
+const Standalone = true

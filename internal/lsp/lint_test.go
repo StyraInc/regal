7ac30f00b9aa5b1e5038a4a13d7346676aa845b2
Anders Eknert <anders@styra.com>
2024-10-16 10:54:34 +0200

    Handle `file://` URLs in exclusion policy
    This is not an ideal solution, as we should not be sending URLs to
    the linter in the first place. But as fixing that is a much broader
    issue, we'll have to settle for a hack in the meantime.
    
    Fixes #1204
    
    Signed-off-by: Anders Eknert <anders@styra.com>

--- internal/lsp/lint_test.go
+++ internal/lsp/lint_test.go
@@ -1,10 +1,14 @@
 package lsp
 
 import (
+	"context"
 	"reflect"
 	"testing"
 
+	"github.com/styrainc/regal/internal/lsp/cache"
 	"github.com/styrainc/regal/internal/lsp/types"
+	"github.com/styrainc/regal/internal/parse"
+	"github.com/styrainc/regal/pkg/config"
 	"github.com/styrainc/regal/pkg/report"
 )
 
@@ -63,5 +67,79 @@ func TestConvertReportToDiagnostics(t *testing.T) {
 
 	if !reflect.DeepEqual(fileDiags, expectedFileDiags) {
 		t.Errorf("Expected file diagnostics: %v, got: %v", expectedFileDiags, fileDiags)
+	}
+}
+
+func TestLintWithConfigIgnoreWildcards(t *testing.T) {
+	t.Parallel()
+
+	conf := &config.Config{
+		Rules: map[string]config.Category{
+			"idiomatic": {
+				"directory-package-mismatch": config.Rule{
+					Level: "ignore",
+				},
+			},
+		},
+	}
+
+	contents := "package p\n\nimport rego.v1\n\ncamelCase := 1\n"
+	module := parse.MustParseModule(contents)
+	workspacePath := "/workspace"
+	fileURI := "file:///workspace/ignore/p.rego"
+	state := cache.NewCache()
+
+	state.SetFileContents(fileURI, contents)
+	state.SetModule(fileURI, module)
+	state.SetFileDiagnostics(fileURI, []types.Diagnostic{})
+
+	if err := updateFileDiagnostics(
+		context.Background(),
+		state,
+		conf,
+		fileURI,
+		workspacePath,
+		[]string{"prefer-snake-case"},
+	); err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	diagnostics, _ := state.GetFileDiagnostics(fileURI)
+	if len(diagnostics) != 1 {
+		t.Fatalf("Expected one diagnostic item, got %v", diagnostics)
+	}
+
+	if diagnostics[0].Code != "prefer-snake-case" {
+		t.Errorf("Expected diagnostic code to be prefer-snake-case, got %v", diagnostics[0].Code)
+	}
+
+	// Clear the diagnostic and update the config with a wildcard ignore
+	// for any file in the ignore directory.
+
+	state.SetFileDiagnostics(fileURI, []types.Diagnostic{})
+
+	conf.Rules["style"] = config.Category{
+		"prefer-snake-case": config.Rule{
+			Level: "error",
+			Ignore: &config.Ignore{
+				Files: []string{"ignore/**"},
+			},
+		},
+	}
+
+	if err := updateFileDiagnostics(
+		context.Background(),
+		state,
+		conf,
+		fileURI,
+		workspacePath,
+		[]string{"prefer-snake-case"},
+	); err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	diagnostics, _ = state.GetFileDiagnostics(fileURI)
+	if len(diagnostics) != 0 {
+		t.Fatalf("Expected no diagnostics, got %v", diagnostics)
 	}
 }

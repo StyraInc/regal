a4589c4da16eea0e85ffa79a905ac659f8c5c308
Charlie Egan <charlie@styra.com>
2025-02-11 12:41:22 +0000

    lsp: Correctly parse eval and debug params
    These had been missed in https://github.com/StyraInc/regal/pull/1382
    
    Signed-off-by: Charlie Egan <charlie@styra.com>

--- internal/lsp/command.go
+++ internal/lsp/command.go
@@ -17,8 +17,16 @@ func toAnySlice(a ...string) *[]any {
 }
 
 type commandArgs struct {
-	Target     string            `json:"target"`
+	// Target is the URI of the document for which the command applies to
+	Target string `json:"target"`
+
+	// Optional arguments, command dependent
+	// Diagnostic is the diagnostic that is to be fixed in the target
 	Diagnostic *types.Diagnostic `json:"diagnostic,omitempty"`
+	// QueryPath is the path of the query to evaluate
+	QueryPath string `json:"path,omitempty"`
+	// Row is the row within the file where the command was run from
+	Row int `json:"row,omitempty"`
 }
 
 func FmtCommand(target string) types.Command {

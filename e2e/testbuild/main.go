6e5e6bb2949327e5d580663d1d8cd27bb9d657de
Stephan Renatus <stephan@styra.com>
2025-06-05 14:28:55 +0200

    e2e: add frankenstein test build
    This is meant to use the Regal version from whatever is in the current
    checkout, together with OPA as defined in Regal's go.mod.
    
    It's an early warning against dependency problems that only become
    apparent when we're using all of OPA, not the subset of its
    functionality that Regal already uses.
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>

---
+++ e2e/testbuild/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/open-policy-agent/opa/cmd"
+	regal_cmd "github.com/styrainc/regal/cmd"
+)
+
+// Create a frankenstein binary: it'll combine all of OPA and Regal.
+// This way, we ensure that every Go module OPA could ever use, when
+// running as server, can be used together with Regal's set of Go
+// modules.
+func main() {
+	for _, c := range regal_cmd.RootCommand.Commands() {
+		cmd.RootCommand.AddCommand(c)
+	}
+	if err := cmd.RootCommand.Execute(); err != nil {
+		panic(err)
+	}
+}

a852bcce445b57ca7caf155e34442fc5e7d64c2e
Anders Eknert <anders@styra.com>
2025-06-30 22:26:23 +0200

    Add Regal bundle compilation benchmark
    This is really testing OPA rather than Regal, but since it's
    a good test case for improving OPA for Regal, it belongs here.
    
    Used in https://github.com/open-policy-agent/opa/pull/7740
    
    Signed-off-by: Anders Eknert <anders@styra.com>

---
+++ internal/compile/compile_bench_test.go
@@ -0,0 +1,21 @@
+package compile
+
+import (
+	"testing"
+
+	"github.com/styrainc/regal/bundle"
+)
+
+// 16	  66555594 ns/op	50239492 B/op	 1083664 allocs/op - main
+// 18	  62569440 ns/op	38723015 B/op	  944277 allocs/op - compiler-optimizations pr
+func BenchmarkCompileBundle(b *testing.B) {
+	bndl := bundle.LoadedBundle
+
+	compiler := NewCompilerWithRegalBuiltins()
+
+	for b.Loop() {
+		if compiler.Compile(bndl.ParsedModules("regal")); len(compiler.Errors) > 0 {
+			b.Fatal(compiler.Errors)
+		}
+	}
+}

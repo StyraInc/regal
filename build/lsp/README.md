89a6a0b16749a06a1b0ebf209431005c650697d7
Stephan Renatus <stephan@styra.com>
2025-07-23 11:25:37 +0200

    build/lsp: add web client demo (#1629)
    This really is what I wanted to do back when contributing build/ws. It's a web-based
    editor using the lovely CodeMirror 6 and its new @codemirror/lsp-client package! ✨

---
+++ build/lsp/README.md
@@ -0,0 +1,8 @@
+# LSP client Web Demo
+
+This is a static HTML page with CodeMirror 6 with its lsp-client, served through esm.sh.
+It uses a Websocket connection to Regal for autocompletion and hover features.
+
+## How to run
+
+Run `go run .` in this directory and visit http://127.0.0.1:8787.
---
+++ build/lsp/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"embed"
+	"log/slog"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+
+	"github.com/styrainc/regal/pkg/config"
+	"github.com/styrainc/regal/pkg/lsp"
+)
+
+//go:embed *.html
+var root embed.FS
+
+func main() {
+	http.Handle("/regal", http.HandlerFunc(handleWS))
+	http.Handle("/", http.FileServerFS(root))
+	panic(http.ListenAndServe(":8787", nil))
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(*http.Request) bool { return true }, // TODO: check something
+}
+
+func handleWS(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+
+	cfg := config.Config{
+		Rules: map[string]config.Category{
+			"idiomatic": {
+				"directory-package-mismatch": config.Rule{
+					Level: "ignore",
+				},
+			},
+			"testing": {
+				"test-outside-test-package": config.Rule{
+					Level: "ignore",
+				},
+				"file-missing-test-suffix": config.Rule{
+					Level: "ignore",
+				},
+			},
+			"imports": {
+				"unresolved-import": config.Rule{
+					Level: "ignore",
+				},
+				"unresolved-reference": config.Rule{
+					Level: "ignore",
+				},
+				"prefer-package-imports": config.Rule{
+					Level: "error",
+					Extra: map[string]any{
+						"ignore-import-paths": []string{"data.system.eopa.utils.tests.v1.filter"},
+					},
+				},
+			},
+		},
+	}
+
+	ls, err := lsp.New(r.Context(), ws, &cfg) // ls takes ownership of ws
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+
+	defer ls.Close() //nolint:errcheck
+	if err := ls.Wait(r.Context()); err != nil {
+		slog.Error(err.Error())
+		return
+	}
+}

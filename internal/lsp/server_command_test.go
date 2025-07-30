82ff6ce4da422e24e6deccdfd0f016e168d14676
Charlie Egan <charlie@styra.com>
2025-07-30 12:37:22 +0000

    lsp: Use full text replacement for Intellij fmt (#1637)
    This client seems to have a bug in applying workspace edits.
    
    Signed-off-by: Charlie Egan <charlie@styra.com>

---
+++ internal/lsp/server_command_test.go
@@ -0,0 +1,163 @@
+package lsp
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/sourcegraph/jsonrpc2"
+
+	"github.com/styrainc/regal/internal/lsp/clients"
+	"github.com/styrainc/regal/internal/lsp/types"
+	"github.com/styrainc/regal/pkg/roast/encoding"
+)
+
+func TestExecuteCommandOpaFmt(t *testing.T) {
+	t.Parallel()
+
+	content := `package files
+
+import data.bar
+allow if {
+1 == 1
+    2 == 2
+    3 == 4
+}
+
+
+
+`
+
+	expectedFormattedContent := `package files
+
+import data.bar
+
+allow if {
+	1 == 1
+	2 == 2
+	3 == 4
+}
+`
+
+	testCases := map[string]struct {
+		clientName    string
+		expectedEdits []types.TextEdit
+	}{
+		"generic client": {
+			clientName:    "go test",
+			expectedEdits: ComputeEdits(content, expectedFormattedContent),
+		},
+		"IntelliJ client": {
+			clientName: "IntelliJ IDEA 2024.2.5",
+			expectedEdits: []types.TextEdit{
+				{
+					Range: types.Range{
+						Start: types.Position{Line: 0, Character: 0},
+						End:   types.Position{Line: 11, Character: 0},
+					},
+					NewText: expectedFormattedContent,
+				},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			tempDir := t.TempDir()
+
+			ctx, cancel := context.WithCancel(t.Context())
+			defer cancel()
+
+			receivedMessages := make(chan types.ApplyWorkspaceEditParams, defaultBufferedChannelSize)
+
+			createWorkspaceApplyEditTestHandler := func(
+				t *testing.T,
+				receivedMessages chan types.ApplyWorkspaceEditParams,
+			) func(_ context.Context, _ *jsonrpc2.Conn, req *jsonrpc2.Request) (result any, err error) {
+				t.Helper()
+
+				return func(_ context.Context, _ *jsonrpc2.Conn, req *jsonrpc2.Request) (result any, err error) {
+					if req.Method == "workspace/applyEdit" {
+						var requestData types.ApplyWorkspaceEditParams
+
+						err = encoding.JSON().Unmarshal(*req.Params, &requestData)
+						if err != nil {
+							t.Fatalf("failed to unmarshal applyEdit params: %s", err)
+						}
+
+						receivedMessages <- requestData
+
+						return map[string]any{"applied": true}, nil
+					}
+
+					t.Fatalf("unexpected request: %v", req)
+
+					return struct{}{}, nil
+				}
+			}
+
+			clientHandler := createWorkspaceApplyEditTestHandler(t, receivedMessages)
+			ls, connClient := createAndInitServer(t, ctx, newTestLogger(t), tempDir, clientHandler)
+
+			// set client identifier for this test since we are testing that behavior
+			ls.clientIdentifier = clients.DetermineClientIdentifier(tc.clientName)
+
+			// edits are sent to the clinet by the command worker
+			go ls.StartCommandWorker(ctx)
+
+			mainRegoURI := fileURIScheme + filepath.Join(tempDir, "main.rego")
+			ls.cache.SetFileContents(mainRegoURI, content)
+
+			executeParams := types.ExecuteCommandParams{
+				Command:   "regal.fix.opa-fmt",
+				Arguments: []any{`{"target":"` + mainRegoURI + `"}`},
+			}
+
+			var executeResponse any
+
+			// simulates a manual fmt request from the client
+			err := connClient.Call(ctx, "workspace/executeCommand", executeParams, &executeResponse)
+			if err != nil {
+				t.Fatalf("failed to execute command: %s", err)
+			}
+
+			timeout := time.NewTimer(determineTimeout())
+			defer timeout.Stop()
+
+			select {
+			case applyEditParams := <-receivedMessages:
+				if applyEditParams.Label != "Format using opa fmt" {
+					t.Fatalf("expected label 'Format using opa fmt', got %s", applyEditParams.Label)
+				}
+
+				if len(applyEditParams.Edit.DocumentChanges) != 1 {
+					t.Fatalf("expected 1 document change, got %d", len(applyEditParams.Edit.DocumentChanges))
+				}
+
+				docChange := applyEditParams.Edit.DocumentChanges[0]
+				if docChange.TextDocument.URI != mainRegoURI {
+					t.Fatalf("expected URI %s, got %s", mainRegoURI, docChange.TextDocument.URI)
+				}
+
+				edits := docChange.Edits
+
+				if len(edits) != len(tc.expectedEdits) {
+					t.Fatalf("expected %d edits, got %d", len(tc.expectedEdits), len(edits))
+				}
+
+				for i, expected := range tc.expectedEdits {
+					actual := edits[i]
+					if actual.Range != expected.Range || actual.NewText != expected.NewText {
+						t.Fatalf("edit %d mismatch:\nexpected: %v\nactual:   %v", i, expected, actual)
+					}
+				}
+
+			case <-timeout.C:
+				t.Fatal("timeout waiting for workspace/applyEdit request")
+			}
+		})
+	}
+}
